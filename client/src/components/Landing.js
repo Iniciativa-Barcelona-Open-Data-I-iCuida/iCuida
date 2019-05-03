29d1734dd2357c64d45884420e770add54b58655
Diana Vilé <diana.vile@gmail.com>
2019-05-03 18:16:24 +0200

    Style Landingpage main container

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,30 +5,103 @@ export default class Landing extends Component {
     render() {
         return(
             <React.Fragment>
+            <main role="main">
                 <div className="container-fluid">
                     <div className="jumbotron mt-5">
                         <h1 className="text-center">iCuida</h1>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
-                        <div className="col-sm-3">.col-sm-3</div>
+                        <p className="text-center">short sentence about APP</p>
                     </div>
                 </div>
-            </React.Fragment>
+            </main>
+                <div className="container"> 
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category1</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category2</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category3</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category4</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category5</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category6</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category7</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category8</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category9</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category10</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category11</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                    <div className="row">  
+                        <div className="col-sm-3"> 
+                            <h2>Category12</h2>
+                            <p>View details</p>
+                            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        </div>
+                    </div>
+                </div>
+        </React.Fragment>   
         )
     }
-}
+}               
+
+   
