4b27e8612f64d4f4e0cfd703d0348731283481d3
Diana Vilé <diana.vile@gmail.com>
2019-05-03 14:48:44 +0200

    Style changes

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import '../assets/css/Landing.css';
 
 export default class Landing extends Component {
     render() {
         return(
-            <div className="container">
-                <div className="jumbotron mt-5">
-                    <h1 className="text-center">Welcome Diana</h1>
+            <React.Fragment>
+                <div className="container">
+                    <div className="jumbotron mt-5">
+                        <h1 className="text-center">Welcome Diana</h1>
+                    </div>
                 </div>
-            </div>
+                <div className="container">
+                    <div className="jumbotron mt-5">
+                        <h1 className="text-center">Welcome Diana</h1>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
