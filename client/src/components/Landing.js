487badcb2066ba85cc426176a9d3f4f7bc4ae93b
Jvincent3 <johnvuc@hotmail.com>
2019-05-03 13:40:45 +0200

    first commit

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Landing extends Component {
+    render() {
+        return(
+            <div className="container">
+                <div className="jumbotron mt-5">
+                    <h1 className="text-center">Welcome Diana</h1>
+                </div>
+            </div>
+        )
+    }
+}
