4dee6ea3e0254edb6de929ced240721bef83b62c
Diana Vilé <diana.vile@gmail.com>
2019-05-07 12:57:38 +0200

    Update Landingpage text and css

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -43,66 +43,85 @@ export default class Landing extends Component {
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category4</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category5</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category6</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category7</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category8</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category9</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category10</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category11</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
                     <div className="row">  
                         <div className="col-md-4"> 
                             <h2>Category12</h2>
-                            <p>View details</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                               Nisl purus in mollis nunc sed id semper risus in. Amet purus gravida quis blandit turpis cursus in hac. 
+                            </p>
                             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
                         </div>
                     </div>
+                    <hr></hr>
                 </div>
             </main>
         </React.Fragment>   
