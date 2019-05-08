612706f38fc66b7d7079cbdddcf3a58d7d6c4204
Diana Vilé <diana.vile@gmail.com>
2019-05-08 11:01:28 +0200

    fix navbar with breadcrumb

--- client/src/Component/Navbar.js
+++ client/src/Component/Navbar.js
@@ -1,78 +1,70 @@
 import React, {Component } from 'react';
 import { Link, withRouter} from 'react-router-dom';
 import '../assets/css/Navbar.css';
+export default class Navbar extends Component{
 
-class Navbar extends Component {
     logOut(e) {
         e.preventDefault()
         localStorage.removeItem('usertoken')
         this.props.history.push('/')
-    }
+        }
 
     render() {
         const loginRegLink = (
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <Link to="/login" className="nav-link">
-                    Login
-                    </Link>
+                    <Link to="/login" className="nav-link">Login</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/register" className="nav-link">
-                        Register
-                    </Link>
+                    <Link to="/register" className="nav-link">Register</Link>
                 </li>
             </ul>
-        )
-
-        const userLink = (
+            )
+    
+    const userLink = (
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <Link to="/profile" className="nav-link">
-                        User
-                    </Link>
+                    <Link to="/profile" className="nav-link">User</Link>
                 </li>
                 <li className="nav-item">
-                    <a href="navlink" onClick={this.logOut.bind(this)} className="nav-link">
-                        Logout
+                    <a href="navlink" 
+                    onClick={this.logOut.bind(this)} className="nav-link">Logout
                     </a>
                 </li>
             </ul>
-        )
+            )
+        
         return(
-            <nav className="navbar navbar-expand-md navbar-dark bg-newcolor">
-                <button className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbar1"
-                aria-controls="navbar1"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-                    <span className="navbar-toggle-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-md-center" id="navbar1">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">
-                                Home
-                            </Link>
-                        </li>
-                    </ul>
-                    {localStorage.usertoken ? userLink : loginRegLink}
-                </div>
+            <React.Fragment>
+                <nav className="navbar navbar-expand-md navbar-dark bg-newcolor">
+                    <button className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbar1"
+                    aria-controls="navbar1"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                        <span className="navbar-toggle-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-md-center"id="navbar1">
+                        <ul className="navbar-nav">
+                            <li className="nav-item"><a to="/" className="nav-link">Home</a></li>
+                        </ul>
+                        {localStorage.usertoken ? userLink : loginRegLink}
+                    </div>
                 <div className="bs-example">
-            <nav> 
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item1"><a href="#Breadcrumb1">1</a></li>
-                    <li className="breadcrumb-item2"><a href="#Breadcrumb2">2</a></li>
-                    <li className="breadcrumb-item3"><a href="#Breadcrumb3">3</a></li>
-                    <li className="breadcrumb-item4"><a href="#Breadcrumb4">4</a></li>
-                </ol>
-            </nav>
-            </div>
-            </nav>
-        )
+                    <nav aria-label="breadcrumb">   
+                        <ol className="breadcrumb">
+                        <li className="breadcrumb-item"><a href="#Breadcrumb1#">Home</a></li>
+                        <li className="breadcrumb-item"><a href="#Breadcrumb2">Login</a></li>
+                        <li className="breadcrumb-item active" aria-current="page">Registrar</li>
+                        </ol>
+                    </nav>
+                </div>
+                </nav>
+            </React.Fragment>
+            )
+        }
     }
-}
-
+        
 export default withRouter(Navbar);
