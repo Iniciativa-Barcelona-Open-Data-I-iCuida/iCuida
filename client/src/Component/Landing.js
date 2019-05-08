00a99d02f67e75bf3dc65f5c94715bffaaa2769c
Diana Vilé <diana.vile@gmail.com>
2019-05-08 19:16:25 +0200

    update search form

--- client/src/Component/Landing.js
+++ client/src/Component/Landing.js
@@ -7,31 +7,35 @@ export default class Landing extends Component {
             <React.Fragment>
             <main role="main">
             <div className="container-fluid">
-                    <div className="jumbotron mt-5">
-                        <div className="search-form">
-                            <nav>
-                                <form className="form-inline">
-                                <div className="input-group"> </div>
-                                <input type="text" className="form-control" placeholder="Search"></input>
-                                <div className="input-group-append"></div>
-                                </form>
-                            </nav>
-                        </div>
+                <div className="jumbotron mt-5">
+                    <div className="search-form">
+                        <nav>
+                            <form className="form-inline">
+                            <div className="input-group">
+                            <input type="text" className="form-control" placeholder="Search"></input> </div>
+                            </form>
+                        </nav>
                     </div>
+                </div>
             </div>
             <div className="container-fluid mt-3"> 
-                <div className="row">  
-                    <div className="col-lg-4 col-md-6 col-xl-3"> 
-                        <h2>Derechos Laborales</h2>
-                        <p>Box1:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Derechos Laborales»</a></p>
-                    </div>
+                <div className="card-group">
+                <div className="card">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h5>Derechos Laborales</h5>
+                    <p className="card-text">Box1:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div> 
+                    <div className="card-footer">
+                    <p><a className="btn btn-secondary" href="#Button1" role="button">Derechos Laborales»</a></p> 
+                </div>              
+            </div>
+
                 <div className="row">  
                     <div className="col-lg-4"> 
                         <h2>Tramites Legales</h2>
                         <p>Box2:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="/img/tramites-legales" alt="tramites-legales" className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Tramites Legales»</a></p>
                     </div>
                 </div>
@@ -39,7 +43,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Seguridad Social</h2>
                         <p>Box3:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="/img/seguridad-social" alt="seguridad-social" className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Seguridad Social»</a></p>
                     </div>
                 </div>      
@@ -47,7 +51,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Extranjería</h2>
                         <p>Box4:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="/img" alt="extranjería" className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Extranjería»</a></p>
                     </div>
                 </div>   
@@ -55,7 +59,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Centros de Suporte</h2>
                         <p>Box5:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Centros de Suporte»</a></p>
                     </div>
                 </div>   
@@ -63,7 +67,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Cuidado de Dependientes</h2>
                         <p>Box6:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Cuidado de Dependientes»</a></p>
                     </div>
                 </div>  
@@ -71,7 +75,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Cuidado de Mayores </h2>
                         <p>Box7:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Cuidado de Mayores»</a></p>
                     </div>
                 </div>  
@@ -79,7 +83,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Canguros</h2>
                         <p>Box8:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Canguros»</a></p>
                     </div>
                 </div> 
@@ -87,7 +91,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Consejos de Limpieza </h2>
                         <p>Box9:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Consejos de Limpieza»</a></p>
                     </div>
                 </div>   
@@ -95,7 +99,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Ayudas Prácticas</h2>
                         <p>Box10:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Ayudas Prácticas»</a></p>
                     </div>
                 </div>  
@@ -103,7 +107,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Recursos de Barcelona</h2>
                         <p>Box11:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Recursos de Barcelona»</a></p>
                     </div>
                 </div>  
@@ -111,7 +115,7 @@ export default class Landing extends Component {
                     <div className="col-lg-4"> 
                         <h2>Otros</h2>
                         <p>Box12:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>
+                         {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
                         <p><a className="btn btn-secondary" href="#Button1" role="button">Otros»</a></p>
                     </div>
                 </div>    
