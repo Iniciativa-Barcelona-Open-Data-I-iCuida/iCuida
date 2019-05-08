aebceda3f4fc4f94d4ce8c3db9ea7482a40dc233
Diana Vilé <diana.vile@gmail.com>
2019-05-08 22:05:01 +0200

    Fix cards

--- client/src/Component/Landing.js
+++ client/src/Component/Landing.js
@@ -20,108 +20,104 @@ export default class Landing extends Component {
             </div>
             <div className="container-fluid mt-3"> 
                 <div className="card-group">
-                <div className="card">  
+                <div className="card1 col-lg-4">  
                 {/*UPDATE CARDS with: <https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/bootstrap-cards.php*/}
                     {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
                 <div className="card-body">
-                    <h5>Derechos Laborales</h5>
+                    <h3>Derechos Laborales</h3>
                     <p className="card-text">Box1:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <p><a className="btn btn-secondary" href="#Button1" role="button">Derechos Laborales»</a></p> 
-                </div> 
-                    <div className="card-footer">
-                    <small class="text-muted">Updated 3 mins ago</small>
-                </div>              
+                </div>             
             </div>
-
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Tramites Legales</h2>
-                        <p>Box2:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="/img/tramites-legales" alt="tramites-legales" className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Tramites Legales»</a></p>
-                    </div>
-                </div>
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Seguridad Social</h2>
-                        <p>Box3:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="/img/seguridad-social" alt="seguridad-social" className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Seguridad Social»</a></p>
-                    </div>
-                </div>      
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Extranjería</h2>
-                        <p>Box4:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="/img" alt="extranjería" className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Extranjería»</a></p>
-                    </div>
-                </div>   
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Centros de Suporte</h2>
-                        <p>Box5:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Centros de Suporte»</a></p>
-                    </div>
-                </div>   
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Cuidado de Dependientes</h2>
-                        <p>Box6:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Cuidado de Dependientes»</a></p>
-                    </div>
-                </div>  
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Cuidado de Mayores </h2>
-                        <p>Box7:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Cuidado de Mayores»</a></p>
-                    </div>
-                </div>  
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Canguros</h2>
-                        <p>Box8:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Canguros»</a></p>
-                    </div>
-                </div> 
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Consejos de Limpieza </h2>
-                        <p>Box9:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Consejos de Limpieza»</a></p>
-                    </div>
-                </div>   
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Ayudas Prácticas</h2>
-                        <p>Box10:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Ayudas Prácticas»</a></p>
-                    </div>
-                </div>  
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Recursos de Barcelona</h2>
-                        <p>Box11:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Recursos de Barcelona»</a></p>
-                    </div>
-                </div>  
-                <div className="row">  
-                    <div className="col-lg-4"> 
-                        <h2>Otros</h2>
-                        <p>Box12:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                         {/*<img src="..." alt="..." className="img-thumbnail rounded-lg float-center"></img>*/}
-                        <p><a className="btn btn-secondary" href="#Button1" role="button">Otros»</a></p>
-                    </div>
-                </div>    
-                    <hr></hr>
+                <div className="card2 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Tramites Legales</h3>
+                    <p className="card-text">Box2:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button2" role="button">Tramites Legales»</a></p> 
+                </div>             
+            </div>
+                <div className="card3 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Seguridad Social</h3>
+                    <p className="card-text">Box3:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button3" role="button">Seguridad Social»</a></p> 
+                </div>             
+            </div>     
+                <div className="card4 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Extranjería</h3>
+                    <p className="card-text">Box4:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button4" role="button">Extranjería»</a></p> 
+                </div>             
+            </div>
+            <div className="card5 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Centros de Suporte</h3>
+                    <p className="card-text">Box5:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Centros de Suporte»</a></p> 
+                </div>             
+            </div> 
+            <div className="card6 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Cuidado de Dependientes</h3>
+                    <p className="card-text">Box6:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Cuidado de Dependientes»</a></p> 
+                </div>             
+            </div> 
+            <div className="card7 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Cuidado de Mayores</h3>
+                    <p className="card-text">Box7:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Cuidado de Mayores»</a></p> 
+                </div>             
+            </div> 
+            <div className="card8 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Canguros</h3>
+                    <p className="card-text">Box8:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Canguros»</a></p> 
+                </div>             
+            </div>  
+            <div className="card9 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Consejos de Limpieza</h3>
+                    <p className="card-text">Box9:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Consejos de Limpieza»</a></p> 
+                </div>             
+            </div>
+            <div className="card10 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Ayudas Prácticas</h3>
+                    <p className="card-text">Box10:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Ayudas Prácticas»</a></p> 
+                </div>             
+            </div>
+            <div className="card11 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Recursos de Barcelona</h3>
+                    <p className="card-text">Box11:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Recursos de Barcelona»</a></p> 
+                </div>             
+            </div>
+            <div className="card12 col-lg-4">  
+                    {/*<img src="/" alt="img-derechos-laborales" className="img-thumbnail rounded-lg float-center"></img>*/}
+                <div className="card-body">
+                    <h3>Otros</h3>
+                    <p className="card-text">Box12:Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a className="btn btn-secondary" href="#Button5" role="button">Otros»</a></p> 
+                </div>             
+            </div>                        
+                <hr></hr>
                 </div>
             </div>
             </main>
