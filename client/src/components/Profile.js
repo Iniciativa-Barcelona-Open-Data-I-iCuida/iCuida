a5c8e17b7ce1aa82bdfad0988ffbe3b0995b5df7
Jvincent3 <johnvuc@hotmail.com>
2019-05-22 03:32:56 +0200

    Implemented profile new style

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import '../assets/css/Profile.css'
 import jwt_decode from 'jwt-decode';
+import Logo from '../assets/images/icono-iCuida-pantone-7416-naranja-01.png';
+
+
 
 class Profile extends Component{
     constructor() {
@@ -13,32 +17,46 @@ class Profile extends Component{
     componentDidMount() {
         const token = localStorage.usertoken;
         const decoded = jwt_decode(token);
+        console.log(decoded)
         this.setState({
-            user_name: decoded.user_name,
+            user_name: decoded.first_name + ' ' + decoded.last_name,
             email: decoded.email
         })
     }
 
     render() {
         return(
-            <div className="container">
-                <div className="jumbotron mt-5">
-                    <div className="col-sm-8 mx-auto">
-                        <h1 className="text-center">PROFILE</h1>
-                    </div>
-                    <table className="table-col-md-6 mx-auto">
-                        <tbody>
-                        <tr>
-                            <td>Nombre de Usuario</td>
-                            <td>{this.state.user_name}</td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td>{this.state.email}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+            <div className="Profile container p-0">
+
+                        <div className="card hovercard">
+                            <div className="cardheader">
+
+                            </div>
+                            <div className="avatar">
+                                <img alt="" src={Logo}/>
+                            </div>
+                            <div className="info">
+                                <div className="user-info">
+
+                                     <p><span className="font-weight-bolder">Nombre: </span><span>iCuida</span></p>
+                                    <p><span className="font-weight-bolder">E-Mail</span>: <span>icuidabarcelona@gmail.com</span></p>
+
+                                </div>
+                            </div>
+                            <div className="bottom mt-3">
+                                <a className="ml-2 mr-2 border rounded-pill p-2" href="">
+                                    <i className="fa fa-twitter"></i>
+                                </a>
+                                <a className="ml-2 mr-2 border rounded-pill p-2" rel="publisher"
+                                   >
+                                    <i className="fa fa-google-plus"></i>
+                                </a>
+                                <a className="ml-2 mr-2 border rounded-pill p-2" rel="publisher"
+                                   >
+                                    <i className="fa fa-facebook"></i>
+                                </a>
+                            </div>
+                        </div>
             </div>
         )
     }
